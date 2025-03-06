edd64bffaccf0c68de1c3c3d10fe59767691fc72
michelle <michelle.graham@dkit.ie>
2025-03-06 13:54:40 +0000

    Feature: Added logic to login, as well as changing font size up and down
    Changes include:
    - Tracking all containers in the structure in a hashmap (key = variable name, value = container) - this was previously all components.
    - Setting a standard font and inserting it to the UI manager
    - Logging in if user provides hard-coded credentials

--- Other/BasicSampleGUI/src/main/java/ui/SampleGUI.java
+++ Other/BasicSampleGUI/src/main/java/ui/SampleGUI.java
@@ -2,12 +2,17 @@ package ui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.HashMap;
 
 public class SampleGUI {
+    // Hard-coded account information:
+    private final String username = "admin";
+    private final String password = "password1";
+
     // GUI components
-    // Create Map of all GUI components
-    private final HashMap<String, Component> guiComponents = new HashMap<>();
+    private final HashMap<String, Container> guiContainers = new HashMap<>();
     // Main gui window
     private JFrame mainFrame;
     // Main Font setting
@@ -27,6 +32,9 @@ public class SampleGUI {
 
     // Use constructor to establish the components (parts) of the GUI
     public SampleGUI() {
+        // Set the standard font settings for the application
+        setStandardFonts();
+
         // Set up the main window
         configureMainWindow();
 
@@ -39,15 +47,14 @@ public class SampleGUI {
         // Create the main frame - this is the main window
         mainFrame = new JFrame("Basic Sample GUI");
         mainFrame.setSize(400, 300);
-        mainFrame.setFont(font);
         // Set what should happen when the X button is clicked on the window
         // This approach will dispose of the main window but not shut down the program
         mainFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         // Set the layout manager used for the main window
         mainFrame.setLayout(new CardLayout());
 
-        // Add to Component map
-        guiComponents.put("mainFrame", mainFrame);
+        // Register the main window as a container in the system
+        guiContainers.put("mainFrame", mainFrame);
     }
 
     // Set up initial panel (initial view)
@@ -56,38 +63,39 @@ public class SampleGUI {
         // This will provide a view to take in the user credentials
         // Use a GridBag layout so we have a grid to work with, but there's some flexibility (button can span columns)
         initialView = new JPanel(new GridBagLayout());
+        // Register this panel as a container in the system
+        guiContainers.put("initialView", initialView);
 
         // Create text fields and associated labels to take in username and password
         // Username info
         usernameLabel = new JLabel("Username: ");
-        usernameLabel.setFont(font);
-        guiComponents.put("usernameLabel", usernameLabel);
         usernameField = new JTextField(15);
-        usernameField.setFont(font);
-        guiComponents.put("usernameField", usernameField);
 
         // Password info
         passwordLabel = new JLabel("Password: ");
-        passwordLabel.setFont(font);
-        guiComponents.put("passwordLabel", passwordLabel);
         passwordField = new JTextField(15);
-        passwordField.setFont(font);
-        guiComponents.put("passwordField", passwordField);
 
         // Create a button to log in user
         loginButton = new JButton("Log in");
-        loginButton.setFont(font);
-        guiComponents.put("loginButton", loginButton);
+        // Specify what the button should DO when clicked:
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                loginUser();
+            }
+        });
 
         // Create a button to increase font
         increaseFontButton = new JButton("Increase Font");
-        increaseFontButton.setFont(font);
-        guiComponents.put("increaseFontButton", increaseFontButton);
+        increaseFontButton.addActionListener(e -> {
+            changeFontSize(1.0f);
+        });
 
         // Create a button to decrease font
         decreaseFontButton = new JButton("Decrease Font");
-        decreaseFontButton.setFont(font);
-        guiComponents.put("decreaseFontButton", decreaseFontButton);
+        decreaseFontButton.addActionListener(e -> {
+            changeFontSize(-1.0f);
+        });
 
         // Add credential components to initial view panel in specific positions within the gridbag
         // Add username label and text field on first row (y = 0)
@@ -137,6 +145,53 @@ public class SampleGUI {
         showInitialView();
     }
 
+    /*
+     * All methods below this point provide application logic
+     */
+    private void loginUser(){
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        if(this.username.equalsIgnoreCase(username)){
+            if(this.password.equals(password)){
+                JOptionPane.showMessageDialog(initialView, "You have successfully logged in!", "Login Successful",
+                        JOptionPane.INFORMATION_MESSAGE);
+                mainFrame.remove(initialView);
+                return;
+            }
+        }
+
+        JOptionPane.showMessageDialog(initialView, "Username/password incorrect. Please try again.", "Login Failed",
+                JOptionPane.ERROR_MESSAGE);
+    }
+
+
+    private void changeFontSize(float sizeChange){
+        font = font.deriveFont(font.getSize()+sizeChange);
+        setStandardFonts();
+        updateContainers();
+        mainFrame.pack();
+    }
+
+    private void setStandardFonts(){
+        UIManager.put("Label.font", font);
+        UIManager.put("TextField.font", font);
+        UIManager.put("Button.font", font);
+        UIManager.put("OptionPane.messageFont", font);
+        UIManager.put("OptionPane.buttonFont", font);
+    }
+
+    private void updateContainers() {
+        for (Container c : guiContainers.values()) {
+            for (Component component : c.getComponents()) {
+                // Set the font in the component
+                component.setFont(font);
+            }
+            // Revalidate and repaint the container
+            c.revalidate();
+            c.repaint();
+        }
+    }
     // GUI runner
     public static void main(String[] args) {
         // Create an instance of the GUI
